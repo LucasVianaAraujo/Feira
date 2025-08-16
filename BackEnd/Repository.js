b33546bb442a2ad8eab8db36aeeca629ff14f20d
Lucas Viana <luks79180@gmail.com>
2025-08-16 11:34:03 -0300

    2.1

--- BackEnd/Repository.js
+++ BackEnd/Repository.js
@@ -1,11 +1,35 @@
 import { connection } from "./Connection.js";
 
 export async function listarUsuarios() {
-    const comando = `
+  const comando = `
     SELECT *
       FROM CADASTRO
   `
 
-    const [registros] = await connection.query(comando)
-    return registros;
+  const [registros] = await connection.query(comando)
+  return registros;
+}
+
+export async function adicionarUsuarios() {
+
+}
+
+export async function inserirUsuario(novoUsuario) {
+  const comando = `
+    INSERT INTO cadastro (NOME, ESCOLARIDADE, INTERESSE_CURSO, PREVISAO_CHEGADA, EMAIL, SABENDO_FEIRA, TELEFONE, ALUNO_FREI, CPF)
+               values (?, ?, ? , ? , ? , ? , ? , ?, ?);
+  `
+
+  const [info] = await connection.query(comando, [
+    novoUsuario.NOME,
+    novoUsuario.ESCOLARIDADE,
+    novoUsuario.INTERESSE_CURSO,
+    novoUsuario.PREVISAO_CHEGADA,
+    novoUsuario.EMAIL,
+    novoUsuario.SABENDO_FEIRA,
+    novoUsuario.TELEFONE,
+    novoUsuario.ALUNO_FREI,
+    novoUsuario.CPF
+  ])
+  return info.insertId;
 }
